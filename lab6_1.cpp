6f8166133515b9ab40c4d1801eba6a0d5a86766a
Kittiwin <kittiwin_phannachet@cmu.ac.th>
2022-12-09 16:03:37 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,29 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int N[1000];
+    int i = 0;
+    int x = 0;
+    int E = 0;
+    int O = 0;
+   cout << "Enter an integer: ";
+   cin >> N[0];
+   while(N[i] > 0){
+        cout << "Enter an integer: ";
+        cin >> N[i+1];
+        i++;
+   }
+
+    while(x <= i){
+        if(N[x]%2 == 0){
+            E = E + 1 ;
+        }else{
+            O = O + 1 ;
+        }
+        x++;
+    }
+    cout << "#Even numbers = " << E - 1 << endl;
+    cout << "#Odd numbers = " << O << endl;
+    
     return 0;
 }
